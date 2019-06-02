72c1db59476622590a15c9f1023f010f416209c3
Carlo Lomod <superloika25@gmail.com>
2019-06-02 22:05:10 +0800

    The hexacrypt file

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# hexacrypt
+# Hexacrypt
+
+
+## About
+Hexacrypt is a prototype of a mini algorithm for basic string encryption/decryption.
+It has a password feature to enhance encryption security.
+
+
+# Usage
+
+Example:
+	
+	from hexacrypt import *
+	
+	msg = "Hello World"
+	security_key = "secret123"
+	
+	encrypted_msg = Hexacrypt.encrypt(msg, security_key)
+	print('encrypted msg: ', encrypted_msg)
+	
+	decrypted_msg = Hexacrypt.decrypt(encrypted_msg, security_key)
+	print('decrypted msg: ', decrypted_msg)
---
+++ hexacrypt.py
@@ -0,0 +1,77 @@
+﻿# hexacrypt.py
+# Hexacrypt
+# Basic String Encryption/Decryption Tool
+# Author: superloika (superloika.github.io)
+
+class Hexacrypt:
+
+    
+
+    def __init__(self):
+        super(Hexacrypt, self).__init__()
+
+    # ENCRYPTION
+    def encrypt(str_to_encrypt, str_key="H3x4Cryp+"):
+        """Author: Kaloi"""
+        if (type(str_to_encrypt) not in (str, chr) or
+                    type(str_key) not in (str, chr)):
+            raise Exception("All args must be a string.")
+        str_to_encrypt = str(str_to_encrypt)
+        j = -1
+        key_len = len(str_key)
+        str_len = len(str_to_encrypt)
+        result = ""
+
+        try:
+            if key_len == 0:
+                str_key = "H3x4Cryp+"
+                key_len = len(str_key)
+            if str_len == 0:
+                # print("No string to encrypt.")
+                return result
+            else:
+                for i in range(str_len):
+                    if i < key_len:
+                        j += 1
+                    else:
+                        j = 0
+                    xored = ord(str_to_encrypt[i]) \
+                            ^ ord(str_key[j])
+                    result += hex(xored)
+        except Exception as e:
+            print("Encryption error: " + str(e))
+        # return "ERROR"
+        return result
+
+    # DECRYPTION
+    def decrypt(hexadecimal_str, str_key="H3x4Cryp+"):
+        """Author: Kaloi"""
+        if (type(hexadecimal_str) not in (str, chr) or
+                    type(str_key) not in (str, chr)):
+            raise Exception("All args must be a string.")
+        lst_str = hexadecimal_str.split("0x")
+        lst_len = len(lst_str)
+        key_len = len(str_key)
+        j = -1
+        result = ""
+        try:
+            if key_len == 0:
+                str_key = "H3x4Cryp+"
+                key_len = len(str_key)
+
+            if lst_len == 1:
+                # print("Unable to parse hexadecimal string.")
+                return result
+            else:
+                for i in range(1, lst_len):
+                    if i <= key_len:
+                        j += 1
+                    else:
+                        j = 0
+                    xored = int(eval("0x" + lst_str[i])) \
+                            ^ ord(str_key[j])
+                    result += chr(xored)
+        except Exception as e:
+            print("Decryption error: " + str(e))
+        # return "ERROR"
+        return result
